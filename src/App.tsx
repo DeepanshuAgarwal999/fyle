5983d7980c7c04dacfbc1574b47295481daed1b3
deepanshu agarwal <deepanshuagarwal9999@gmail.com>
2024-06-03 18:16:14 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/fyle/' element={<Home />} />
+        <Route path='*' element={<>Invalid route</>} />
+
       </Routes>
     </div>
   )
